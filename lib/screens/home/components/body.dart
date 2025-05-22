90d0c24d836e64cde994d361119eca669b277ba6
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-22 18:56:58 +0700

    Add- untuk membuat layout 40%

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constants.dart';
 import 'package:plantapp/screens/home/components/header_with_searchbox.dart';
+import 'package:plantapp/screens/home/components/title_with_more_btn.dart';
 
 class BodyPage extends StatelessWidget {
   const BodyPage({super.key});
@@ -20,45 +21,8 @@ class BodyPage extends StatelessWidget {
               print("Lihat lebih banyak tanaman");
             },
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithMoreBtn extends StatelessWidget {
-  const TitleWithMoreBtn({super.key, required this.title, required this.press});
-
-  final String title;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Row(
-        children: [
-          TitleWithCustomUnderline(text: title),
-          Spacer(),
-          TextButton(
-            // Solusi terbaik
-            style: TextButton.styleFrom(
-              backgroundColor: kPrimaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-              minimumSize: Size(70, 36),
-            ),
-            onPressed: press,
-            child: Text(
-              'More',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
+          Container(
+            width: size.width * 0.4,
           ),
         ],
       ),
@@ -66,39 +30,4 @@ class TitleWithMoreBtn extends StatelessWidget {
   }
 }
 
-class TitleWithCustomUnderline extends StatelessWidget {
-  const TitleWithCustomUnderline({super.key, required this.text});
 
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              margin: EdgeInsets.only(
-                left: kDefaultPadding / 4,
-                right: kDefaultPadding,
-              ),
-              height: 3,
-              color: kPrimaryColor,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}

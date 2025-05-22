dd13d54cd9f8785f044e2d3497259d783639b112
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-22 20:47:39 +0700

    Add- Card feuture plants

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constants.dart';
 import 'package:plantapp/screens/home/components/header_with_searchbox.dart';
+import 'package:plantapp/screens/home/components/recomend_plants.dart';
 import 'package:plantapp/screens/home/components/title_with_more_btn.dart';
 
 class BodyPage extends StatelessWidget {
@@ -22,161 +23,55 @@ class BodyPage extends StatelessWidget {
             },
           ),
           RecomendsPlants(),
+          TitleWithMoreBtn(title: "Feature Plants", press: (){}),
+          Row(
+            children: <Widget>[
+              FeaturePlantsCards(size: size,
+              image: "assets/images/tanaman5.jpg",
+              press:(){},
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
 }
 
-class RecomendsPlants extends StatelessWidget {
-  const RecomendsPlants({
+class FeaturePlantsCards extends StatelessWidget {
+  const FeaturePlantsCards({
     super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          RecomendPlantCard(
-            image: "assets/images/tanaman1.jpeg",
-            title: "Bulu atu",
-            country: "Indonesia",
-            price: 440,
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailsScreen(),
-                ),
-              );
-            },
-          ),
-      
-          RecomendPlantCard(
-            image: "assets/images/tanaman2.jpg",
-            title: "Bulu bata",
-            country: "Indonesia",
-            price: 540,
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailsScreen(),
-                ),
-              );
-            },
-          ),
-      
-          RecomendPlantCard(
-            image: "assets/images/tanaman4.jpg",
-            title: "Bulu ratu",
-            country: "Indonesia",
-            price: 540,
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailsScreen(),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RecomendPlantCard extends StatelessWidget {
-  const RecomendPlantCard({
-    super.key, 
-    required this.image, 
-    required this.title, 
-    required this.country, 
-    required this.price, 
+    required this.image,
+    required this.size,
     required this.press,
-    
   });
 
-  final String image, title, country;
-  final int price;
-  final Function press;
-
+  final Size size;
+  final String image;
+  final VoidCallback press; 
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        left: kDefaultPadding,
-        top: kDefaultPadding / 2,
-        bottom: kDefaultPadding * 2.5,
-      ),
-      width: size.width * 0.4,
-      child: Column(
-        children: <Widget>[
-          Image.asset(image),
-          GestureDetector(
-            onTap: press,
-            child: Container(
-              padding: EdgeInsets.all(kDefaultPadding / 2),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft:Radius.circular(10),
-                  bottomRight:Radius.circular(10),
-                  ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "$title\n".toUpperCase(),
-                          style: Theme.of(
-                            context,
-                          ).textTheme.labelLarge?.copyWith(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                
-                        TextSpan(
-                          text: "$country".toUpperCase(),
-                          style: Theme.of(
-                            context,
-                          ).textTheme.labelLarge?.copyWith(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    '\$$price',
-                    style: TextStyle(
-                      fontSize: 14, // Sesuaikan ukuran
-                      fontWeight: FontWeight.bold,
-                      color: kPrimaryColor,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press ,
+      child: Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding/2,
+          bottom: kDefaultPadding / 2,
+         ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit:BoxFit.cover,
+            image: AssetImage(image)
+          )
+        ),
       ),
     );
   }
 }
+

0374b436e0b86b1ab1b93404265b9d7d4ec3bedc
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-22 16:43:10 +0700

    Add-wrap widget

--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,27 +9,30 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: kPrimaryColor,
-        centerTitle: true, 
-        leadingWidth: 48, 
-        leading: IconButton(
-          onPressed: (){},
-          icon: SvgPicture.asset(
-            "assets/icons/icons8-menu.svg",
-            color: Colors.white, 
-          ),
-        ),
-        title: Text(
-          "Home",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: Container(), // Tambahkan body kosong jika diperlukan
+      appBar: buildAppBar(),
+      body: BodyPage(), // Tambahkan body kosong jika diperlukan
     );
   }
 }
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: kPrimaryColor,
+      centerTitle: true, 
+      leadingWidth: 48, 
+      leading: IconButton(
+        onPressed: (){},
+        icon: SvgPicture.asset(
+          "assets/icons/icons8-menu.svg",
+          color: Colors.white, 
+        ),
+      ),
+      title: Text(
+        "Home",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }

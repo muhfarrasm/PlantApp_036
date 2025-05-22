eabcedd6be81f942b6437743a2a72d7c5b8c4e94
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-22 17:29:48 +0700

    Add - Text diatas test box search

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constants.dart';
 
 class BodyPage extends StatelessWidget {
@@ -15,11 +16,26 @@ class BodyPage extends StatelessWidget {
             children: <Widget>[
               Container(
                 height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(color: kPrimaryColor,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(36),
-                  bottomRight: Radius.circular(36)
-                )
+                decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(36),
+                    bottomRight: Radius.circular(36),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.only(left: kDefaultPadding),
+                      child: Text(
+                      'Hi Farras',
+                      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ),
+                    
+                  ],
                 ),
               ),
               Positioned(
@@ -27,38 +43,47 @@ class BodyPage extends StatelessWidget {
                 left: 0,
                 right: 0,
                 child: Container(
-                margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                height: 54,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0, 10),
-                      blurRadius: 50,
-                      color: kPrimaryColor,
-                    ),
-                  ],
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search",
-                    hintStyle: TextStyle(
-                      color: kPrimaryColor,
-                    ),
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    contentPadding: EdgeInsets.only(left: 20),
-                     
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            hintStyle: TextStyle(color: kPrimaryColor),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            contentPadding: EdgeInsets.only(left: 20),
+                          ),
+                        ),
+                      ),
+                       Padding( // Tambahkan Padding untuk ikon
+                        padding: EdgeInsets.only(right: 15),
+                        child: SvgPicture.asset(
+                          "assets/icons/search.svg",
+                          color: kPrimaryColor, // Sesuaikan warna
+                          height: 20, // Atur ukuran
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-
-
-              ),
               ),
             ],
           ),
-        )
+        ),
       ],
     );
   }

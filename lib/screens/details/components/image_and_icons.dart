a964bdce622f5e6fc7fd4104aa78331fc88c2855
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-23 01:07:13 +0700

    Add-image_and_icons

--- lib/screens/details/components/image_and_icons.dart
+++ lib/screens/details/components/image_and_icons.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:plantapp/constants.dart';
 import 'package:plantapp/screens/details/components/icon_card.dart';
 
-class Body extends StatelessWidget {
-  const Body({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          ImagesandCons(size: size),
-        ],
-      ),
-    );
-  }
-}
 
 class ImagesandCons extends StatelessWidget {
   const ImagesandCons({

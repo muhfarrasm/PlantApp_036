ab933fc023172f1bd5a55d7a80b7960319c28dd9
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-22 20:12:09 +0700

    Add-Plant Cards

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -21,26 +21,19 @@ class BodyPage extends StatelessWidget {
               print("Lihat lebih banyak tanaman");
             },
           ),
-          Container(
-            width: size.width * 0.4,
-            child: Column(
-              children: <Widget>[
-                Image.asset("assets/images/tanaman1.jpeg"),
-                Container(
-                  padding: EdgeInsets.all(kDefaultPadding / 2),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      ),
-                    ],
-                    ),
+          RecomendPlantCard(
+            image: "assets/images/tanaman1.jpeg",
+            title: "Bulu atu",
+            country: "Indonesia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
                 ),
-              ],
-            ),
+              );
+            },
           ),
         ],
       ),
@@ -48,4 +41,94 @@ class BodyPage extends StatelessWidget {
   }
 }
 
+class RecomendPlantCard extends StatelessWidget {
+  const RecomendPlantCard({
+    super.key, 
+    required this.image, 
+    required this.title, 
+    required this.country, 
+    required this.price, 
+    required this.press,
+    
+  });
 
+  final String image, title, country;
+  final int price;
+  final Function press;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft:Radius.circular(10),
+                  bottomRight:Radius.circular(10),
+                  ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(
+                            context,
+                          ).textTheme.labelLarge?.copyWith(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                
+                        TextSpan(
+                          text: "$country".toUpperCase(),
+                          style: Theme.of(
+                            context,
+                          ).textTheme.labelLarge?.copyWith(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    '\$$price',
+                    style: TextStyle(
+                      fontSize: 14, // Sesuaikan ukuran
+                      fontWeight: FontWeight.bold,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
